24e813116219d98fe4c6fe4ada5b0a5b26a2c92b
sdigiovann <s.di-giovanni@hotmail.fr>
2016-03-29 15:16:21 +0200

    MAJ Question2

--- B32XX.c
+++ B32XX.c
@@ -14,18 +14,22 @@ int is_prime(uint64_t p)
 	return premier;
 }
 
+//QUESTION 2
 void print_prime_factors(uint64_t n)
 {	printf("%d:", n);
 	uint64_t i=2;
-	while (i<n)
+	while (i<=n)
 	{	if (n%i==0)
-		{	printf("%d ", i);
-		}
-		i++;
+		{	printf(" %d", i);
+			n=n/i;
+		} else
+		{	i++;
+		}	
 	}
+	printf("\r\n");
 }
 
 int main ()
-{	print_prime_factors(15);
+{	print_prime_factors(12488);
 	return 0;
 }
